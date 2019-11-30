8dc96e59d4df1e73d290fd68cfa8b9ac69cd9163
LeoVS09 <leovs09@gmail.com>
2019-11-30 11:59:37 +0300

    refactro structure and add markdown

---
+++ bot.ts
@@ -0,0 +1,58 @@
+import Telegraf  from 'telegraf'
+import Extra from  'telegraf/extra'
+import Markup from 'telegraf/markup'
+import db from './lib/db'
+
+const token = process.env.TELEGRAM_TOKEN
+const bot = new Telegraf(token);
+
+const markup = Extra.markdown()
+const keyboard = Markup.keyboard([
+  ['List']
+])    
+  .oneTime()
+  .resize();
+
+const baseOptions = {
+    parse_mode: 'Markdown'
+}
+
+bot.start((ctx) => ctx.reply('Hello'))
+bot.hears('hi', ctx => ctx.reply('Hey there!', Extra.markup(keyboard)))
+
+bot.hears(/Add (.+)/, async ({match, reply, chat}) => {
+  const text = match[1]
+
+  await db.create(chat.id, text)
+
+  reply(`${text} *successfully added!*`, markup)
+})
+
+bot.hears('List', async ({reply, chat}) => {
+  reply('*TODO*:', markup)
+
+  for await (const todo of db.list()) {
+    if(todo.chatId === chat.id)
+      reply(todo.text)
+  }
+})
+
+bot.hears(/Done (.+)/, async ({match, reply, chat}) => {
+  const text = match[1]
+
+  for await (const todo of db.list()) {
+
+    if(todo.chatId !== chat.id || todo.text !== text) 
+      continue
+
+    await db.delete(todo.id)
+
+    reply(`${todo.text} *completed!*`, markup)
+  }
+
+})
+
+bot.help((ctx) => ctx.reply('Help message'))
+bot.hears('ping', ctx => ctx.reply('pong'))
+
+export default (body) => bot.handleUpdate(body)
--- handler.ts
+++ handler.ts
@@ -1,61 +1,13 @@
 import 'source-map-support/register'
 import wrapLambda from './lib/api'
-import Telegraf  from 'telegraf'
-import Extra from  'telegraf/extra'
-import Markup from 'telegraf/markup'
-import db from './lib/db'
+import botHandler from './bot'
 
-const token = process.env.TELEGRAM_TOKEN
-const bot = new Telegraf(token);
 
-const keyboard = Markup.keyboard([
-  ['List']
-])    
-  .oneTime()
-  .resize();
-
-bot.start((ctx) => ctx.reply('Hello'))
-bot.hears('hi', ctx => ctx.reply('Hey there!', Extra.markup(keyboard)))
-
-bot.hears(/Add (.+)/, async ({match, reply, chat}) => {
-  const text = match[1]
-
-  await db.create(chat.id, text)
-
-  reply('Successfully added!')
-})
-
-bot.hears('List', async ({reply, chat}) => {
-  reply('TODO:')
-
-  for await (const todo of db.list()) {
-    if(todo.chatId === chat.id)
-      reply(todo.text)
-  }
-})
-
-bot.hears(/Done (.+)/, async ({match, reply, chat}) => {
-  const text = match[1]
-
-  for await (const todo of db.list()) {
-
-    if(todo.chatId !== chat.id || todo.text !== text) 
-      continue
-
-    await db.delete(todo.id)
-
-    reply(`Todo: ${todo.text} completed!`)
-  }
-
-})
-
-bot.help((ctx) => ctx.reply('Help message'))
-bot.action('ping', ctx => ctx.reply('pong'))
 
 export const webhook = wrapLambda(async (event, _context) => {
   
   const tmp = JSON.parse(event.body)
-  await bot.handleUpdate(tmp)
+  await botHandler(tmp)
 
   return {
     statusCode: 200,
